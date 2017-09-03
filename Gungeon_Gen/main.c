eb3d59db76366f95a8da0b296450c63b1bcf6316
Juanven3gas <juanven3gas@gmail.com>
2017-09-03 18:52:28 -0500

    Implementing add room
    celaning up code & trying to implement the add room function

--- Gungeon_Gen/main.c
+++ Gungeon_Gen/main.c
@@ -7,28 +7,51 @@
 #define num_rooms 5
 
 char dungeon[dungeon_rows][dungeon_columns];
+char rooms[5][4];
+
+void add_rooms(void);
+void init_rooms(void);
+void init_dungeon(void);
+void print_dungeon(void);
+
+int main()
+{
+    init_dungeon();
+    //add_rooms();
+    print_dungeon();
+
+    return 0;
+}
 
 void add_rooms(void)
 {
-    int i, j , k;
+    int i, j;
+    int start_row, start_column;
+    int rows, columns;
 
     srand(time(NULL));
 
-    for(i = 0; i < num_rooms; i++)
+    rows = (rand() % 7) + 3;
+    columns = (rand() % 7) + 2;
+    start_row = (rand() % 24) + 1;
+    start_column = (rand() % 81) + 1;
+
+    printf("room rows: %d, room columns: %d, start_row: %d, start_column: %d\n", rows, columns, start_row, start_column);
+
+    for(i = start_row; i < (start_row + rows); i++)
     {
-        int x_dim = (rand() % 6) + 3;
-        int y_dim = (rand() % 6) + 2;
-
-        //Check the perimeter
-        for(j = 0; j < x_dim; j++)
+        for(j = start_column; j < (start_column + columns); j++)
         {
-            for(k = 0; k < y_dim; k++)
-            {
-
-            }
+            dungeon[i][j] = '.';
         }
-
     }
+}
+
+void init_rooms(void)
+{
+    srand(time(NULL));
+
+
 }
 
 void init_dungeon(void)
@@ -63,12 +86,4 @@ void print_dungeon(void)
         printf("\n");
     }
 
-}
-
-int main()
-{
-    init_dungeon();
-    print_dungeon();
-
-    return 0;
 }
