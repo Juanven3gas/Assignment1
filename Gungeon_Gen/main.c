7e0f144af367dfae5f34b144b86f9fb1c3d3996f
Juanven3gas <juanven3gas@gmail.com>
2017-09-04 13:24:44 -0500

    IMplementing check room
    Checking to see if there is sufficient space between rooms

--- Gungeon_Gen/main.c
+++ Gungeon_Gen/main.c
@@ -7,17 +7,17 @@
 #define num_rooms 5
 
 char dungeon[dungeon_rows][dungeon_columns];
-char rooms[5][4];
+int rooms[5][4];
 
 void add_rooms(void);
-void init_rooms(void);
+void add_corridors(void);
 void init_dungeon(void);
 void print_dungeon(void);
 
 int main()
 {
     init_dungeon();
-    //add_rooms();
+    add_rooms();
     print_dungeon();
 
     return 0;
@@ -25,34 +25,75 @@ int main()
 
 void add_rooms(void)
 {
-    int i, j;
+    int room_counter;
     int start_row, start_column;
     int rows, columns;
 
+    int i, j;
+    room_counter = 0;
+
     srand(time(NULL));
 
-    rows = (rand() % 7) + 3;
-    columns = (rand() % 7) + 2;
-    start_row = (rand() % 24) + 1;
-    start_column = (rand() % 81) + 1;
-
-    printf("room rows: %d, room columns: %d, start_row: %d, start_column: %d\n", rows, columns, start_row, start_column);
-
-    for(i = start_row; i < (start_row + rows); i++)
+    while(room_counter < 5)
     {
-        for(j = start_column; j < (start_column + columns); j++)
+        int success = 1;
+
+        //random row from 3 - 9
+        rows = (rand() % 7) + 3;
+
+        //random col from 3 - 9
+        columns = (rand() % 7) + 3;
+
+        //random row from 1 - 12
+        start_row = (rand() % 11) + 1;
+
+        //random col from 1 - 72
+        start_column = (rand() % 71) + 1;
+
+        /*
+         * Check to see if the room is by
+         * another room by 3 spaces
+         * if so then generate another room
+         */
+        for(i = start_row; i < (start_row + rows); i++)
         {
-            dungeon[i][j] = '.';
+            for(j = start_column; j < (start_column + columns); j++)
+            {
+                if(dungeon[i][j] == '.')
+                if(dungeon[i][j] == '.')
+                {
+                    success = 0;
+                    break;
+                }
+            }
         }
+
+        /*
+         * Add room to the dungeon & then increment the room counter
+         */
+        if(success)
+        {
+            printf("room rows: %d, room columns: %d, start_row: %d, start_column: %d\n", rows, columns, start_row, start_column);
+
+            for(i = start_row; i < (start_row + rows); i++)
+            {
+                for(j = start_column; j < (start_column + columns); j++)
+                {
+                    dungeon[i][j] = '.';
+                }
+            }
+
+            rooms[room_counter][0] = rows;
+            rooms[room_counter][1] = columns;
+            rooms[room_counter][2] = start_row;
+            rooms[room_counter][3] = start_column;
+
+            room_counter++;
+        }
+
     }
 }
 
-void init_rooms(void)
-{
-    srand(time(NULL));
-
-
-}
 
 void init_dungeon(void)
 {
